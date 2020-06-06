713e48d7fa566a524189134023531fe13698bcf5
Paul Narbonneau <pnarbonn@gmail.com>
2020-06-06 19:02:11 +0200

    wip

---
+++ mpl-service/src/main/resources/data.sql
@@ -0,0 +1,14 @@
+drop table if exists users;
+create table users (
+    id int auto_increment primary key,
+    nom varchar2(250) not null,
+    prenom varchar2(250) not null,
+    email varchar2(250),
+    adresse1 varchar2(250) not null,
+    adresse2 varchar2(250),
+    codepostal varchar2(5) not null,
+    ville varchar2(100) not null
+);
+
+insert into users (nom,prenom,email,adresse1,adresse2,codepostal,ville) 
+values ('Narbonneau','Paul','pnarbonn@gmail.com','17 allée de l''ile marante','','92700','colombes');
---
+++ mpl-ui/src/app/components/login/login.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormControl, Validators } from '@angular/forms';
+
+import * as CryptoJs from 'crypto-js';
+
+@Component({
+  selector: 'mpl-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  loginFormGroup = new FormGroup(
+    {
+      user : new FormControl('',Validators.required),
+      password : new FormControl('',Validators.required)
+    }
+  );
+
+  constructor() { 
+  }
+
+  ngOnInit(): void {
+    this.showStatus();
+  }
+
+  onSubmit()
+  {
+    console.log(JSON.stringify(this.loginFormGroup.value));
+    console.log(`Username : ${this.loginFormGroup.value.user}`);
+    console.log(`password : ${CryptoJs.AES.encrypt(this.loginFormGroup.value.user,"doudou")}`);
+  }
+
+  showStatus()
+  {
+    console.log(this.user);
+    console.log(this.user.invalid && (this.user.dirty || this.user.touched));
+    console.log(this.user.errors?.required);
+  }
+
+  get password() { return this.loginFormGroup.get('password');}
+
+  get user() { return this.loginFormGroup.get('user');}
+
+}

6b85ba3cb5d898759bc813d78af0e16b7cd291ac
Paul Narbonneau <pnarbonn@gmail.com>
2020-06-07 20:05:40 +0200

    wip

--- mpl-ui/src/app/app.component.ts
+++ mpl-ui/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'mpl-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
+  ngOnInit(): void {
+
+    
+  }
+  
+
+  playAudio()
+  {
+    
+    let audio = new Audio();
+    audio.src= "/assets/sounds/Bourriquet.mp3";
+    audio.play();
+    console.log("chargement audio");
+  }
   title = 'mpl-ui';
+
+
+
+
 }
